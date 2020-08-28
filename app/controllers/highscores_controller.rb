8ee3b7c86d130cda221c6de5ae9291d7fc5dac96
codewithirene567 <irenejinscott@gmail.com>
2020-08-28 14:37:29 -0400

    added high score params and added a create action

--- app/controllers/highscores_controller.rb
+++ app/controllers/highscores_controller.rb
@@ -4,9 +4,23 @@ class HighscoresController < ApplicationController
         render json: HighscoreSerializer.new(highscores).to_serialized_json
       end
 
+      def create
+          highscore = Highscore.new(highscore_params)
+          highscore.save
+          highscores = []
+          highscores.push(highscore)
+          render json: HighscoreSerializer.new(highscores)
+
+      end
+
       def show
         highscore = Highscore.find_by(id: params[:id])
         render json: HighscoreSerializer.new(highscore).to_serialized_json
 
       end
+
+      def highscore_params
+        params.require(:highscore).permit(:name)
+      end
+
 end
