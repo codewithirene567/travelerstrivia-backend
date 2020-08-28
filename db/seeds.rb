4fee140c04e88b8009f8225f01ddb8c0c44a6712
codewithirene567 <irenejinscott@gmail.com>
2020-08-27 20:22:19 -0400

    added another country in the seeds, and reseeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,17 @@
 china = Country.create(name: "China")
 
 
-china_question_1= Question.create(text: "How many people live there?", answer1: "a", answer2: "b", answer3: "c", right: "a", country: china )
-
+china_question_1= Question.create(text: "How many people live there?", answer1: "1.393 billion", answer2: "1 billion", answer3: "500 million", right: "1.393 billion", country: china )
+china_question_2=Question.create(text: "How big is the land?", answer1: "4,705,410 sq mi", answer2: "3,705,410 sq mi", answer3: "2,705,410 sq mi", right: "3,705,410 sq mi", country: china )
+china_question_3= Question.create(text: "What percent of people live in urban areas?", answer1: "50%", answer2: "47%", answer3: "20%", right: "47%", country: china )
 
 highscore_for_china = Highscore.create(name: "samplename", score:"10/10", country: china)
+
+india = Country.create(name: "India")
+
+
+india_question_1= Question.create(text: "Which of these games was invented there?", answer1: "Chess", answer2: "Checkers", answer3: "Horseback riding", right: "Chess", country: india )
+india_question_2=Question.create(text: "Which modern concept was first started in India?", answer1: "Universities", answer2: "The metric system", answer3: "Farming", right: "Universities", country: india )
+india_question_3= Question.create(text: "India has the largest number of which of the following in the world?", answer1: "States", answer2: "Farmers", answer3: "Vegetarians", right: "Vegetarians", country: india )
+
+highscore_for_india = Highscore.create(name: "winner", score:"10/10", country: india)
